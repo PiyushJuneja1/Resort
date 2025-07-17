18e07f0483c15ac84e2677b89fa021e6d0f32408
PiyushJuneja1 <piyushjuneja06470@gmail.com>
2025-07-17 12:43:50 +0530

    Adding Resort class

--- Solution.java
+++ Solution.java
@@ -15,5 +15,31 @@ class Resort{
     public void setResortId(int resortId){
         this.resortId = resortId;
     }
-    
+    public int getResortId(){
+        return resortId;
+    }
+    public void setResortName(String resortName){
+        this.resortName=resortName;
+    }
+    public String getResortName(){
+        return resortName;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public void setPrice(double price){
+        this.price=price;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public void setRating(double rating){
+        this.rating=rating;
+    }
+    public double getRating(){
+        return rating;
+    }
 }
