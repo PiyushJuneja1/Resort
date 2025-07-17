1f5c096bf3e619df0758c2e695eb5643a769faab
PiyushJuneja1 <piyushjuneja06470@gmail.com>
2025-07-17 12:41:25 +0530

    Adding Resort class

---
+++ Solution.java
@@ -0,0 +1,19 @@
+
+public class Solution{
+    public static void main(String[] args) {
+        
+    }
+}
+class Resort{
+
+    private int resortId;
+    private String resortName;
+    private String category;
+    private double price;
+    private double rating;
+
+    public void setResortId(int resortId){
+        this.resortId = resortId;
+    }
+    
+}
